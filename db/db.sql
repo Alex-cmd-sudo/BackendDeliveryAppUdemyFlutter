b37ce5d6c3071262b7481f3a5b42e65338b6d95c
zapa1194 <zapa1194@gmail.com>
2023-02-13 00:24:02 -0600

    Nuevas tablas

--- db/db.sql
+++ db/db.sql
@@ -12,3 +12,61 @@ CREATE TABLE users
     created_at TIMESTAMP(0) NOT NULL,
     updated_at TIMESTAMP(0) NOT NULL
 );
+
+CREATE TABLE roles(
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    image VARCHAR(255) NULL,
+    route VARCHAR(180) NOT NULL,
+    created_at TIMESTAMP(0) NOT NULL,
+    updated_at TIMESTAMP(0) NOT NULL
+);
+
+INSERT INTO roles(
+	name,
+    route,
+    created_at,
+    updated_at
+)
+VALUES(
+	'RESTAURANTE',
+    '/restaurant/orders/list',
+    '2022-02-13',
+    '2022-02-13'
+);
+
+INSERT INTO roles(
+	name,
+    route,
+    created_at,
+    updated_at
+)
+VALUES(
+	'REPARTIDOR',
+    '/delivery/orders/list',
+    '2022-02-13',
+    '2022-02-13'
+);
+
+INSERT INTO roles(
+	name,
+    route,
+    created_at,
+    updated_at
+)
+VALUES(
+	'CLIENTE',
+    '/client/products/list',
+    '2022-02-13',
+    '2022-02-13'
+);
+
+CREATE TABLE user_has_roles(
+	id_user BIGINT NOT NULL,    
+    id_rol BIGINT NOT NULL,    
+    created_at TIMESTAMP(0) NOT NULL,
+    updated_at TIMESTAMP(0) NOT NULL,
+    FOREIGN KEY(id_user) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY(id_rol) REFERENCES roles(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    PRIMARY KEY(id_user, id_rol)
+);
